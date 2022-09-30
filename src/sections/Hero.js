bda5655aa173660a359481e39081e4d6261b5446
Sebastian Balaj <darius.balaj@icloud.com>
2022-09-30 08:12:57 +0300

    fixed typo

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="flex items-center justify-center flex-col min-h-screen max-w-6xl mx-auto">
       <img src={logo} alt="Sriptex" />
-      <h1 className="text-white text-7xl leading-tight">Cooming soon.</h1>
+      <h1 className="text-white text-7xl leading-tight">Coming soon.</h1>
       <h1 className="text-white text-3xl">Stay connected, Stay updated!</h1>
     </section>
   );
